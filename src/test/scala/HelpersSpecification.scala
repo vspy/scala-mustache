defaf6a2acfa191a6c1da9cc59c1eb30e44bda1e
Victor Bilyk <victorbilyk@gmail.com>
2010-12-26 02:10:36 +0600

    parent helpers test

--- src/test/scala/HelpersSpecification.scala
+++ src/test/scala/HelpersSpecification.scala
@@ -69,6 +69,29 @@ object HelperSpecification extends SpecificationWithJUnit {
       )).toString must be equalTo("""<ul><li><img src="https://secure.gravatar.com/avatar/fbf7c6aec1d4280b7c2704c1c0478bd6?s=30">alice</li><li><img src="https://secure.gravatar.com/avatar/10ac39056a4b6f1f6804d724518ff2dc?s=30">bob</li></ul>""")
     }
 
+    "render values returned by parent helper" in {
+      val userList = new Mustache(
+        "<ul>" +
+          "{{# users}}" +
+            "<li><img src=\"{{ gravatar }}\">{{ login }}</li>" +
+          "{{/ users}}" +
+        "</ul>"
+      )
+      val page = new Mustache("<html><body>{{>userList}}</body></html>")
+      val root = new GravatarMustacheExample(true, "{{>content}}")
+
+      root.render(Map(
+          "users"->List(
+            Map("email"->"alice@example.org"
+                ,"login"->"alice")
+            ,Map("email"->"bob@example.org"
+                ,"login"->"bob")
+          )
+        ), Map("content"->page, "userList"->userList)
+      ) must be equalTo("""<html><body><ul><li><img src="https://secure.gravatar.com/avatar/fbf7c6aec1d4280b7c2704c1c0478bd6?s=30">alice</li><li><img src="https://secure.gravatar.com/avatar/10ac39056a4b6f1f6804d724518ff2dc?s=30">bob</li></ul></body></html>""")
+
+    }
+
   }
 
 }
